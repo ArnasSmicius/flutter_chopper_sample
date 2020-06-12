3edeaad3bd9ab2791d8907c6694b8d470f7171d3
Arnas Smicius <henrikas.smicius@vinted.com>
2020-06-12 20:51:16 +0300

    Implement the UI

---
+++ lib/home_page.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:chopper/chopper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chopper_sample/api/post_api_service.dart';
+import 'package:flutter_chopper_sample/single_post_page.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chopper Blog'),
+      ),
+      body: _buildBody(context),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          final service = Provider.of<PostApiService>(context, listen: false);
+          final response = await service.postPost({'key': 'value'});
+          print(response.body);
+        },
+      ),
+    );
+  }
+
+  FutureBuilder<Response> _buildBody(BuildContext context) {
+    // FutureBuilder is perfect for easily building UI when awaiting a Future
+    // Response is the type currently returned by all the methods of PostApiService
+    return FutureBuilder<Response>(
+      future: Provider.of<PostApiService>(context).getPosts(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // Snapshot's data is the Response
+          // You can see there's no type safety here (only List<dynamic>)
+          final List posts = json.decode(snapshot.data.bodyString);
+          return _buildPosts(context, posts);
+        }
+      },
+    );
+  }
+
+  ListView _buildPosts(BuildContext context, List posts) {
+    return ListView.builder(
+      itemCount: posts.length,
+      padding: EdgeInsets.all(8),
+      itemBuilder: (context, index) {
+        return Card(
+          elevation: 4,
+          child: ListTile(
+            title: Text(
+              posts[index]['title'],
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(posts[index]['body']),
+            onTap: () => _navigateToPost(context, posts[index]['id']),
+          ),
+        );
+      },
+    );
+  }
+
+  void _navigateToPost(BuildContext context, int id) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => SinglePostPage(postId: id),
+    ));
+  }
+}
