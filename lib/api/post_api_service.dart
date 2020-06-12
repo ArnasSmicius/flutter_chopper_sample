1ad67e102fe1805b0851845dad041fa0cbaddb5e
Arnas Smicius <henrikas.smicius@vinted.com>
2020-06-12 20:16:18 +0300

    Implement Post API service

---
+++ lib/api/post_api_service.dart
@@ -0,0 +1,31 @@
+import 'package:chopper/chopper.dart';
+
+// Source code generation in Dart works by creating a new file which contains a "companion class".
+// In order for the source gen to know which file to generate and which files are "linked",
+// you need to use the part keywork.
+part 'post_api_service.chopper.dart';
+
+@ChopperApi(baseUrl: '/posts')
+abstract class PostApiService extends ChopperService {
+  @Get()
+  Future<Response> getPosts();
+
+  @Get(path: '/{id}')
+  Future<Response> getPost(@Path('id') int id);
+
+  @Post()
+  Future<Response> postPost(
+    @Body() Map<String, dynamic> body,
+  );
+
+  static PostApiService create() {
+    final client = ChopperClient(
+      baseUrl: 'https://jsonplaceholder.typicode.com',
+      services: [
+        _$PostApiService(),
+      ],
+      converter: JsonConverter(),
+    );
+    return _$PostApiService(client);
+  }
+}
